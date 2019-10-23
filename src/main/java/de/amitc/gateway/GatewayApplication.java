ef4295a95a0a32ac665146a904e5ca9ab3f68c23
Andreas Maier <a.maier@amitc.de>
2019-10-24 00:28:51 +0300

    added dynamic route covering

--- src/main/java/de/amitc/gateway/GatewayApplication.java
+++ src/main/java/de/amitc/gateway/GatewayApplication.java
@@ -2,7 +2,11 @@ package de.amitc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,11 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient, DiscoveryLocatorProperties props) {		
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, props);
 	}
 
 }
